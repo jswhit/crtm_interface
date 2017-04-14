27000407cb21667d4c7e52546eaeaac8b0370bf2
jswhit <whitaker.jeffrey@gmail.com>
2017-04-14 20:02:36 +0000

    update

--- test.py
+++ test.py
@@ -2,4 +2,8 @@ import crtm_interface
 isis='amsua_n15'
 obstype='amsua'
 crtm_coeffs_path = '/scratch3/BMC/gsienkf/whitaker/gsi/EXP-enkflinhx/fix/crtm-2.2.3/'
-crtm_interface.crtm_initialize(1,2,3,7,isis,obstype,crtm_coeffs_path)
+init_pass=1
+mype_diaghdr=2
+mype=3
+nchanl=7
+crtm_interface.crtm_initialize(init_pass,mype_diaghdr,mype,nchanl,isis,obstype,crtm_coeffs_path)
