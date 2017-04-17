a6888571dbe8853574824d0321600b3ad82908bf
jswhit <whitaker.jeffrey@gmail.com>
2017-04-17 14:28:07 +0000

    create Channel_Info object

--- crtm_interface.pyx
+++ crtm_interface.pyx
@@ -17,6 +17,19 @@ def crtm_strlen():
 
 _strlen = crtm_strlen()
 
+cdef class Channel_Info(object):
+    cdef public object n_Channels,Sensor_ID,Sensor_Type,WMO_Satellite_ID,WMO_Sensor_ID
+    cdef public object Process_Channel,Sensor_Channel,Channel_Index
+    def __init__(self,sensor_id,nchannels,sensor_type,wmo_sat_id,wmo_sensor_id,process_channel,sensor_channel,channel_index):
+        self.n_Channels = nchannels
+        self.Sensor_ID = sensor_id
+        self.Sensor_Type = sensor_type
+        self.WMO_Satellite_ID = wmo_sat_id
+        self.WMO_Sensor_ID = wmo_sensor_id
+        self.Process_Channel = process_channel
+        self.Sensor_Channel = sensor_channel
+        self.Channel_Index = channel_index
+
 def crtm_initialize(int nchanl, char *isis, int iload_cloudcoeff, int iload_aerosolcoeff, char *crtm_coeffs_path):
     cdef int nchar_isis, nchar_path
     cdef int sensor_type, wmo_sat_id, wmo_sensor_id
@@ -26,4 +39,4 @@ def crtm_initialize(int nchanl, char *isis, int iload_cloudcoeff, int iload_aero
     nchar_isis = len(isis)
     nchar_path = len(crtm_coeffs_path)
     init_crtm(&nchanl, isis, &nchar_isis, &iload_cloudcoeff, &iload_aerosolcoeff, crtm_coeffs_path, &nchar_path, &sensor_type, &wmo_sat_id, &wmo_sensor_id, <char *>process_channel.data, <int *>sensor_channel.data, <int *>channel_index.data)
-    return sensor_type,wmo_sat_id,wmo_sensor_id,process_channel,sensor_channel,channel_index
+    return Channel_Info(isis,nchanl,sensor_type,wmo_sat_id,wmo_sensor_id,process_channel,sensor_channel,channel_index)
--- test.py
+++ test.py
@@ -4,9 +4,10 @@ crtm_coeffs_path = '/scratch3/BMC/gsienkf/whitaker/gsi/EXP-enkflinhx/fix/crtm-2.
 nchanl = 15
 iload_cloudcoeffs=1
 iload_aerosolcoeffs=1
-sensor_type,wmo_satid,wmo_sensor_id,process_channel,sensor_channel,channel_index = crtm_interface.crtm_initialize(nchanl,isis,iload_cloudcoeffs,iload_aerosolcoeffs,crtm_coeffs_path)
-print 'sensor_type,wmo_satid,wmo_sensor_id',sensor_type,wmo_satid,wmo_sensor_id
-print 'process_channel',process_channel
-print 'sensor_channel',sensor_channel
-print 'channel_index',channel_index
-print 'crtm strlen:',crtm_interface.crtm_strlen()
+channel_info = crtm_interface.crtm_initialize(nchanl,isis,iload_cloudcoeffs,iload_aerosolcoeffs,crtm_coeffs_path)
+print 'sensor_id:',channel_info.Sensor_ID
+print 'sensor_type,wmo_satid,wmo_sensor_id=',channel_info.Sensor_Type,channel_info.WMO_Satellite_ID,channel_info.WMO_Sensor_ID
+print 'process_channel=',channel_info.Process_Channel
+print 'sensor_channel=',channel_info.Sensor_Channel
+print 'channel_index=',channel_info.Channel_Index
+print 'crtm strlen=',crtm_interface.crtm_strlen()
